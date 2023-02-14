37bde30a110c168f2d7b89a05f712edf75580da7
TTranchese <tommaso.tranchese.96@gmail.com>
2023-02-14 15:30:25 +0100

    tommaso

--- src/main/java/Converter.java
+++ src/main/java/Converter.java
@@ -2,15 +2,15 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-public class Converter <T extends Number>{
+public class Converter<S extends Number, T extends Number> {
 
-    public List<Double> toDouble(List<T> list) {
-        List<Double> doubleList = new ArrayList<>(list.size());
-        for(Number number : list) {
-            doubleList.add(number.doubleValue());
-        }
-        return doubleList;
-    }
-
+	public List<T> toDouble(List<S> list) {
+		List<T> convertedList = new ArrayList<>(list.size());
+		for (S number : list) {
+			convertedList.add((T) number);
+		}
+		System.out.println(convertedList);
+		return convertedList;
+	}
 }
 
