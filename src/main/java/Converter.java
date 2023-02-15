b4ac3fd44d94bc751f5e35baa13ec33cb7c3dc54
aegroto <aegroto@protonmail.com>
2023-02-15 10:46:43 +0100

    cleanup

--- src/main/java/Converter.java
+++ src/main/java/Converter.java
@@ -1,16 +1,3 @@
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-
-public class Converter<S extends Number, T extends Number> {
-
-	public List<T> toDouble(List<S> list) {
-		List<T> convertedList = new ArrayList<>(list.size());
-		for (S number : list) {
-			convertedList.add((T) number);
-		}
-		System.out.println(convertedList);
-		return convertedList;
-	}
+public class Converter {
 }
 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,5 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class Main {
     public static void main(String[] args) {
-        List<Integer> integersList = List.of(5, 2, 3);
-        System.out.println(toDouble(integersList));
 
-        List<Long> longsList = List.of(5L, 2L, 3L);
-        System.out.println(toDouble(longsList));
-
-        Converter<Double, Integer> converter = new Converter<>();
-        System.out.println(converter.toDouble(List.of(2.3,3.3,1.1)));
-
-    }
-
-    public static List<Double> toDouble(List<? extends Number> list) {
-        List<Double> doubleList = new ArrayList<>(list.size());
-        for(Number number : list) {
-            doubleList.add(number.doubleValue());
-        }
-        return doubleList;
     }
 }
